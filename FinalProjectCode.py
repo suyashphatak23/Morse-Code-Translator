57b8a6bf9bd97766a54d1a0d0c97660bbea43e52
Suyash Phatak <52849472+suyashphatak23@users.noreply.github.com>
2019-11-10 16:37:57 +0530

    uploading files on GitHub from local PC

---
+++ FinalProjectCode.py
@@ -0,0 +1,39 @@
+#PROGRAMMING AND PROBLEM SOLVING PROJECT
+
+#BY SUYASH PHATAK
+#PRN : 19070122125
+#BATCH: CS C2
+
+#MORSE CODE TRANSLATOR
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+   'C':'-.-.', 'D':'-..', 'E':'.',
+   'F':'..-.', 'G':'--.', 'H':'....',
+   'I':'..', 'J':'.---', 'K':'-.-',
+   'L':'.-..', 'M':'--', 'N':'-.',
+   'O':'---', 'P':'.--.', 'Q':'--.-',
+   'R':'.-.', 'S':'...', 'T':'-',
+   'U':'..-', 'V':'...-', 'W':'.--',
+   'X':'-..-', 'Y':'-.--', 'Z':'--..',
+   '1':'.----', '2':'..---', '3':'...--',
+   '4':'....-', '5':'.....', '6':'-....',
+   '7':'--...', '8':'---..', '9':'----.',
+   '0':'-----', ', ':'--..--', '.':'.-.-.-',
+   '?':'..--..', '/':'-..-.', '-':'-....-',
+   '(':'-.--.', ')':'-.--.-'
+}
+def morsify(text):
+   morse_code = ''
+   for x in text:
+      if x != ' ':
+         morse_code += MORSE_CODE_DICT[x] + ' '
+      else:
+         morse_code += ' '
+   return morse_code
+def main():
+   my_text = input("Enter a string in English to convert in Morse code:")
+   output = morsify(my_text.upper())
+   print ('Your Morse code is ' + output)
+
+if __name__ == '__main__':
+   main()
